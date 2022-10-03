dae801d9945904a884cbb88df91a6f2e8c26cfc7
hdavidlp <heclop@hotmail.com>
2022-10-03 15:35:52 -0700

    API ASP.NET to manage the post Data

---
+++ aspnetserver/Program.cs
@@ -0,0 +1,94 @@
+using aspnetserver.Data;
+using aspnetserver.Repositories.Post;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+//using Microsoft.AspNetCore.Http;
+//using Swashbuckle.AspNetCore.SwaggerUI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(swaggerGenOptions => 
+{
+    swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo { Title = "ASP.Net React Tutorial", Version = "v1" });
+
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(swaggerUIOptions =>
+{
+    swaggerUIOptions.DocumentTitle = "ASP.Net React Tutorial";
+    swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API serving a very simple PostModel");
+    swaggerUIOptions.RoutePrefix = String.Empty;
+});
+
+app.UseHttpsRedirection();
+
+app.MapGet ("/get-all-posts", async () => await PostRepository.GetPostsAsync())
+    .WithTags("Post Endpoints");
+
+app.MapGet("/get-post-by-id/{postId}", async (int postId) =>
+    {
+        Post postToReturn = await PostRepository.GetPostByIdAsync(postId);
+    
+        if (postToReturn!= null)
+        {
+            return Results.Ok(postToReturn);
+        }
+        else
+        {
+            return Results.BadRequest();
+        }
+    }).WithTags("Post Endpoints");
+
+
+app.MapPost("/create-post", async (Post postToCreate) =>
+{
+    bool createSuccessful = await PostRepository.CreatePostAsync(postToCreate);
+
+    if (createSuccessful)
+    {
+        return Results.Ok("Create successful");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Post Endpoints");
+
+app.MapPut ("/update-post", async (Post postToUpdate) =>
+{
+    bool updateSuccessful = await PostRepository.UpdatePostAsync(postToUpdate);
+
+    if (updateSuccessful)
+    {
+        return Results.Ok("Update successful");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Post Endpoints");
+
+
+app.MapDelete("/delete-post-by-id/{postId}", async (int postId) =>
+{
+    bool deleteSuccessful = await PostRepository.DelePostAsync(postId);
+
+    if (deleteSuccessful)
+    {
+        return Results.Ok("Delete successful");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Post Endpoints");
+
+
+
+app.Run();
---
+++ aspnetserver/Repositories/Post/PostRepository.cs
@@ -0,0 +1,74 @@
+﻿using aspnetserver.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetserver.Repositories.Post
+{
+    internal static class PostRepository
+    {
+        internal async static Task<List<Data.Post>> GetPostsAsync()
+        {
+            using (var db = new AppDBContext())
+            {
+                return await db.Posts.ToListAsync();
+            }
+        }
+
+        internal async static Task<Data.Post> GetPostByIdAsync(int postId)
+        {
+            using (var db = new AppDBContext())
+            {
+                return await db.Posts
+                    .FirstOrDefaultAsync(post => post.PostId == postId);
+            }
+        }
+
+        internal async static Task<bool> CreatePostAsync(Data.Post postToCreate)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    await db.Posts.AddAsync(postToCreate);
+                    return await db.SaveChangesAsync()>=1;
+                }
+                catch(Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+        internal async static Task<bool> UpdatePostAsync(Data.Post postToUpdate)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    db.Posts.Update(postToUpdate);
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+
+        internal async static Task<bool> DelePostAsync(int PostId)
+        {
+            using (var db = new AppDBContext())
+            {
+                try
+                {
+                    Data.Post postToDelete = await GetPostByIdAsync(PostId);
+                    db.Posts.Remove(postToDelete);
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+}
